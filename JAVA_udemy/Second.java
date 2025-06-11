397df75d24dfbc8ad60ae99f55a9e29135659576
Swasti <swastinirawade@gmail.com>
2025-06-12 01:17:45 +0530

    I learned how to read input from the keyboard using the Scanner class in Java.

---
+++ JAVA_udemy/Second.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.*;
+
+class Second 
+{
+  public static void main(String args[])
+  { 
+   Scanner s = new Scanner(System.in);
+   int a, b, c;
+   System.out.println("Enter 2 no's.");
+   a = s.nextInt();
+   b = s.nextInt();
+   c = a+b;
+   System.out.println("sum is." +c );
+  }
+}
---
+++ JAVA_udemy/Third.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+
+class Third 
+{
+  public static void main(String args[])
+  { 
+   Scanner s = new Scanner(System.in);
+   String name;
+   System.out.println("Enter your name.");
+   name = s.nextLine();
+   System.out.println("Hello" +name );
+  }
+}
