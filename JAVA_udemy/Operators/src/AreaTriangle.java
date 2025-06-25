9400bd6a67b6917ecbd9ee9fd28e514a26f61c80
Swasti <swastinirawade@gmail.com>
2025-06-25 15:04:28 +0530

    To compute area of Triangle from three sides : -Accepts sides a, b, and c via user input. -Calculates semi-perimeter (p) and area using Heron's formula. -Demonstrates use of Math.sqrt() and float/double handling.

---
+++ JAVA_udemy/Operators/src/AreaTriangle.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class AreaTriangle
+{
+    public static void main(String args[]) {
+        float a, b, c;
+        double p, area;
+        System.out.println("Enter values for a ,b ,c  ");
+        Scanner s = new Scanner(System.in);
+        a = s.nextFloat();
+        b = s.nextFloat();
+        c = s.nextFloat();
+        p = (a + b + c) / 2f ;
+        System.out.println("p =" +p);
+        area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        System.out.println("Area of the Triangle =" +area);
+
+    }
+}
