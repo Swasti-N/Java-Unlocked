4eef5ab43e4716c12a0f437f67f010f384f301f0
Swasti <swastinirawade@gmail.com>
2025-06-25 15:05:40 +0530

    To compute area of triangle based on user input: -Uses Scanner to read base and height from the console. -Calculates area using the formula: 0.5 * base * height. -Demonstrates use of float and double data types.

---
+++ JAVA_udemy/Operators/src/AreaTriangle2.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class AreaTriangle2
+{
+    public static void main(String args[])
+    {
+        float base , height ;
+        double area ;
+        System.out.println("Enter Base and Height  ");
+        Scanner s = new Scanner(System.in);
+        base = s.nextFloat();
+        height = s.nextFloat();
+        area = 0.5 * base * height ; // base * height/2 or 1f / 2f * base * heigth
+        System.out.println("area of triangle is "+area);
+    }
+}
